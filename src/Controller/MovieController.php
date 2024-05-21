675f2c6d83d03efd4ff2d2c36e8f0251ddb06f02
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-05-21 16:33:26 +0200

    Prepare exercise

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/movie')]
+class MovieController extends AbstractController
+{
+    #[Route('', name: 'app_movie_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('movie/index.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_movie_show', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        $movie = [
+            'id' => $id,
+            'title' => 'Star Wars - Episode IV : A New Hope',
+            'country' => 'United States',
+            'plot' => 'A young farmer breaks his fathers toys.',
+            'releasedAt' => new \DateTimeImmutable('25/05/1977'),
+            'genres' => ['Action', 'Adventure', 'Fantasy'],
+        ];
+
+        return $this->render('movie/show.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+}
