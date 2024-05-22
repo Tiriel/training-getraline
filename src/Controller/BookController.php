d56d409e83952a62afabde914a622eda08c323c8
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-05-22 12:04:39 +0200

    Requests in DB

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,24 +14,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class BookController extends AbstractController
 {
     #[Route('', name: 'app_book_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(BookRepository $repository, Request $request): Response
     {
+        $limit = 9;
+        $page = $request->query->getInt('page', 1);
+        $books = $repository->findBy([], [], $limit, ($page - 1) * $limit);
+
         return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
+            'books' => $books,
         ]);
     }
 
-    #[Route(
-        '/{id}',
-        name: 'app_book_show',
-        requirements: ['id' => '\d+'],
-        defaults: ['id' => 0],
-        methods: ['GET'],
-    )]
-    public function show(int $id = 1): Response
+    #[Route('/{id}', name: 'app_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Book $book): Response
     {
         return $this->render('book/show.html.twig', [
-            'controller_name' => 'BookController - id = '.$id,
+            'book' => $book,
         ]);
     }
 }
--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,25 +13,20 @@ use Symfony\Component\Routing\Attribute\Route;
 class MovieController extends AbstractController
 {
     #[Route('', name: 'app_movie_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(MovieRepository $repository, Request $request): Response
     {
+        $limit = 9;
+        $page = $request->query->getInt('page', 1);
+        $movies = $repository->findBy([], [], $limit, ($page - 1) * $limit);
+
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $movies,
         ]);
     }
 
     #[Route('/{id}', name: 'app_movie_show', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function show(int $id): Response
+    public function show(?Movie $movie): Response
     {
-        $movie = [
-            'id' => $id,
-            'title' => 'Star Wars - Episode IV : A New Hope',
-            'country' => 'United States',
-            'plot' => 'A young farmer breaks his fathers toys.',
-            'releasedAt' => new \DateTimeImmutable('25-05-1977'),
-            'genres' => ['Action', 'Adventure', 'Fantasy'],
-        ];
-
         return $this->render('movie/show.html.twig', [
             'movie' => $movie,
         ]);
