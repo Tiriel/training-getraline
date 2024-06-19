ea8e5490b97e57f675b48e06b0d18de5b9028fe0
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-06-19 14:14:51 +0200

    Env var

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello/{name?World}', name: 'app_hello', requirements: ['name' => '(?:\p{L}|[- ])+'])]
-    public function index(string $name): Response
+    public function index(string $name, #[Autowire(param: 'app.sf_constraint')] $toto): Response
     {
+        dump($toto);
         return $this->render('hello/index.html.twig', [
             'controller_name' => $name,
         ]);
